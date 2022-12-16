25943a024bb5a3426ca87b5b650763def3dc4aab
Julien Gordon <junigord@gmail.com>
2022-12-16 15:42:49 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Technical test on portfolio attribution data analysis and visualization for National Bank',
+    author='Julien N Gordon',
+    license='MIT',
+)
